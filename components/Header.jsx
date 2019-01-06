5e4b1401e11e23271fc2e40051869bf0b99c570f
Sumit Govil <sumit.govil@optus.com.au>
2019-01-07 09:13:28 +1100

    NProgress and SS styled component support

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 import styled from "styled-components";
-import Nav from "./Nav";
 import Link from "next/link";
+import Router from "next/router";
+import NProgress from "nprogress";
+import Nav from "./Nav";
 
 const Logo = styled.h1`
   font-size: 4rem;
@@ -40,6 +42,16 @@ const StyledHeader = styled.header`
     border-bottom: 1px solid ${props => props.theme.lightgrey};
   }
 `;
+
+Router.onRouteChangeStart = () => {
+  NProgress.start();
+};
+Router.onRouteChangeComplete = () => {
+  NProgress.done();
+};
+Router.onRouteChangeError = () => {
+  NProgress.done();
+};
 
 const Header = () => {
   return (
