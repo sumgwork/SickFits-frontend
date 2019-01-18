fd6a03d844ccf92419c7edd60680340aeab1821b
Sumit Govil <sumit.govil@optus.com.au>
2019-01-18 18:53:15 +1100

    retrieving logged in user

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,20 @@
 import Link from "next/link";
 import React from "react";
 import NavStyles from "./styles/NavStyles";
+import User from "./User";
 
 const Nav = props => {
   return (
     <NavStyles>
+      <User>
+        {user => {
+          const { me } = user.data;
+          if (!me) {
+            return null;
+          }
+          return <p>{me.name}</p>;
+        }}
+      </User>
       <Link href="/items">
         <a>Shop</a>
       </Link>
--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,17 @@
 import Signup from "../components/Signup";
+import styled from "styled-components";
 
-const signupPage = () => <Signup />;
+const Columns = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(300px, 1fr));
+  grid-gap: 20px;
+`;
+const signupPage = props => (
+  <Columns>
+    <Signup />
+    <Signup />
+    <Signup />
+  </Columns>
+);
 
 export default signupPage;
