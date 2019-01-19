64a8afdefb0bf0ec9f74f0a7eb1ac70942f9d347
Sumit Govil <sumit.govil@optus.com.au>
2019-01-20 10:49:49 +1100

    created table for managing permissions

--- components/Permissions.jsx
+++ components/Permissions.jsx
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Error from "./ErrorMessage";
 import { Query } from "react-apollo";
 import gql from "graphql-tag";
+import Table from "./styles/Table";
+import ShopButton from "./styles/ShopButton";
 
 const ALL_USERS_QUERY = gql`
   query ALL_USERS_QUERY {
@@ -14,6 +16,15 @@ const ALL_USERS_QUERY = gql`
   }
 `;
 
+const possiblePermissions = [
+  "ADMIN",
+  "USER",
+  "ITEMCREATE",
+  "ITEMUPDATE",
+  "ITEMDELETE",
+  "PERMISSIONUPDATE"
+];
+
 class Permissions extends Component {
   state = {};
   render() {
@@ -24,11 +35,50 @@ class Permissions extends Component {
           return (
             <div>
               <Error error={error} />
-              <p>Permissions</p>
+              <h2>Manage Permissions</h2>
+              <Table>
+                <thead>
+                  <tr>
+                    <th>User</th>
+                    <th>Email</th>
+                    {possiblePermissions.map(permission => (
+                      <th key={permission}>{permission}</th>
+                    ))}
+                    <th>👇</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {data &&
+                    data.users &&
+                    data.users.map(user => <User key={user.id} user={user} />)}
+                </tbody>
+              </Table>
             </div>
           );
         }}
       </Query>
+    );
+  }
+}
+
+class User extends Component {
+  render() {
+    const { user } = this.props;
+    return (
+      <tr>
+        <td>{user.name}</td>
+        <td>{user.email}</td>
+        {possiblePermissions.map(permission => (
+          <td key={permission}>
+            <label htmlFor={`${user.id}-${permission}`}>
+              <input type="checkbox" />
+            </label>
+          </td>
+        ))}
+        <td>
+          <ShopButton>Update</ShopButton>
+        </td>
+      </tr>
     );
   }
 }
