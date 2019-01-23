030de73f892da5070acd10d374b9ac80828fbf2a
Sumit Govil <sumit.govil@optus.com.au>
2019-01-23 21:43:01 +1100

    used downshift to enhance search component

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import Router from "next/router";
 import { ApolloConsumer } from "react-apollo";
-import Downshift from "downshift";
+import Downshift, { resetIdCounter } from "downshift";
 import gql from "graphql-tag";
 import debounce from "lodash.debounce";
 import { DropDown, DropDownItem, SearchStyles } from "./styles/DropDown";
@@ -22,6 +22,13 @@ const SEARCH_ITEMS_QUERY = gql`
     }
   }
 `;
+
+function routeToItem(item) {
+  Router.push({
+    pathname: "/item",
+    query: { id: item.id }
+  });
+}
 
 class AutoComplete extends Component {
   state = {
@@ -44,30 +51,57 @@ class AutoComplete extends Component {
     });
   }, 350);
   render() {
+    resetIdCounter();
     return (
       <SearchStyles>
-        <div>
-          <ApolloConsumer>
-            {client => (
-              <input
-                type="search"
-                placeholder="Search for an item"
-                onChange={e => {
-                  e.persist();
-                  this.onChange(e, client);
-                }}
-              />
-            )}
-          </ApolloConsumer>
-          <DropDown>
-            {this.state.items.map(item => (
-              <DropDownItem key={item.id}>
-                <img width="50" src={item.image} alt={item.title} />
-                <p>{item.title}</p>
-              </DropDownItem>
-            ))}
-          </DropDown>
-        </div>
+        <Downshift
+          onChange={routeToItem}
+          itemToString={item => (item === null ? "" : item.title)}
+        >
+          {({
+            getInputProps,
+            getItemProps,
+            highlightedIndex,
+            isOpen,
+            inputValue
+          }) => (
+            <div>
+              <ApolloConsumer>
+                {client => (
+                  <input
+                    {...getInputProps({
+                      onChange: e => {
+                        e.persist();
+                        this.onChange(e, client);
+                      },
+                      placeholder: "Search for an item",
+                      type: "search",
+                      id: "search",
+                      className: this.state.loading ? "loading" : ""
+                    })}
+                  />
+                )}
+              </ApolloConsumer>
+              {isOpen && (
+                <DropDown>
+                  {this.state.items.map((item, index) => (
+                    <DropDownItem
+                      {...getItemProps({ item })}
+                      key={item.id}
+                      highlighted={index === highlightedIndex}
+                    >
+                      <img width="50" src={item.image} alt={item.title} />
+                      <p>{item.title}</p>
+                    </DropDownItem>
+                  ))}
+                  {!this.state.items.length && !this.state.loading && (
+                    <DropDownItem>No matching item</DropDownItem>
+                  )}
+                </DropDown>
+              )}
+            </div>
+          )}
+        </Downshift>
       </SearchStyles>
     );
   }
--- components/styles/DropDown.js
+++ components/styles/DropDown.js
@@ -1,4 +1,4 @@
-import styled, { keyframes } from 'styled-components';
+import styled, { keyframes } from "styled-components";
 
 const DropDown = styled.div`
   position: absolute;
@@ -9,13 +9,14 @@ const DropDown = styled.div`
 
 const DropDownItem = styled.div`
   border-bottom: 1px solid ${props => props.theme.lightgrey};
-  background: ${props => (props.highlighted ? '#f7f7f7' : 'white')};
+  background: ${props => (props.highlighted ? "#f7f7f7" : "white")};
   padding: 1rem;
   transition: all 0.2s;
-  ${props => (props.highlighted ? 'padding-left: 2rem;' : null)};
+  ${props => (props.highlighted ? "padding-left: 2rem;" : null)};
   display: flex;
   align-items: center;
-  border-left: 10px solid ${props => (props.highlighted ? props.theme.lightgrey : 'white')};
+  border-left: 10px solid
+    ${props => (props.highlighted ? props.theme.lightgrey : "white")};
   img {
     margin-right: 10px;
   }
@@ -23,11 +24,11 @@ const DropDownItem = styled.div`
 
 const glow = keyframes`
   from {
-    box-shadow: 0 0 0px yellow;
+    box-shadow: 5 2 0px red;
   }
 
   to {
-    box-shadow: 0 0 10px 1px yellow;
+    box-shadow: 0 0 10px 1px red;
   }
 `;
 
