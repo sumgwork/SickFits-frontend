9d0ecf48f97d4c9d5478877925f991a573f1fadd
Sumit Govil <sumit.govil@optus.com.au>
2019-01-23 21:16:17 +1100

    created search bar and dropdown (to implement downshift next)

--- components/Search.jsx
+++ components/Search.jsx
@@ -2,18 +2,70 @@ import React, { Component } from "react";
 import Router from "next/router";
 import { ApolloConsumer } from "react-apollo";
 import Downshift from "downshift";
-import gql from "graphql";
+import gql from "graphql-tag";
 import debounce from "lodash.debounce";
 import { DropDown, DropDownItem, SearchStyles } from "./styles/DropDown";
 
+const SEARCH_ITEMS_QUERY = gql`
+  query SEARCH_ITEMS_QUERY($searchTerm: String!) {
+    items(
+      where: {
+        OR: [
+          { title_contains: $searchTerm }
+          { description_contains: $searchTerm }
+        ]
+      }
+    ) {
+      id
+      image
+      title
+    }
+  }
+`;
+
 class AutoComplete extends Component {
+  state = {
+    items: [],
+    loading: false
+  };
+  //debounce is used to wait for intermediate events to cease
+  onChange = debounce(async (e, client) => {
+    this.setState({ loading: true });
+    //Manually query Apollo client
+    const res = await client.query({
+      query: SEARCH_ITEMS_QUERY,
+      variables: {
+        searchTerm: e.target.value
+      }
+    });
+    this.setState({
+      items: res.data.items,
+      loading: false
+    });
+  }, 350);
   render() {
     return (
       <SearchStyles>
         <div>
-          <input type="search" />
+          <ApolloConsumer>
+            {client => (
+              <input
+                type="search"
+                placeholder="Search for an item"
+                onChange={e => {
+                  e.persist();
+                  this.onChange(e, client);
+                }}
+              />
+            )}
+          </ApolloConsumer>
           <DropDown>
-            <p>Items will go here!</p>
+            {this.state.items.map(item => (
+              <DropDownItem key={item.id}>
+                <img width="50" src={item.image} alt={item.title} />
+                <p>{item.title}</p>
+              </DropDownItem>
+            ))}
           </DropDown>
         </div>
       </SearchStyles>
